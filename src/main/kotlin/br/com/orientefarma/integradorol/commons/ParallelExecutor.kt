9dc7f32dfa2bfd9f61f9c87de2a669dd71f02085
Guilherme Alves <g.policarto@gmail.com>
2023-06-14 16:38:34 -0300

    Melhoria para garantir que somente 1 pool de thread será criado.

--- src/main/kotlin/br/com/orientefarma/integradorol/commons/ParallelExecutor.kt
+++ src/main/kotlin/br/com/orientefarma/integradorol/commons/ParallelExecutor.kt
@@ -1,23 +1,35 @@
 package br.com.orientefarma.integradorol.commons
 
+import java.util.*
 import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicBoolean
 
 data class Task(val name: String, val runnable: Runnable)
 
 class ParallelExecutor private constructor(threadPoolSize: Int) {
     private val executor = Executors.newFixedThreadPool(threadPoolSize)
-    private val tasks: MutableList<Task> = ArrayList()
+    private val tasks: Queue<Task> = LinkedList()
+    private val estaExecutando: AtomicBoolean = AtomicBoolean(false)
 
     companion object {
         private var instance: ParallelExecutor? = null
 
         @Synchronized
         fun getInstance(threadPoolSize: Int): ParallelExecutor {
-            if (instance == null || instance!!.executor.isShutdown) {
+            if (instance == null) {
                 instance = ParallelExecutor(threadPoolSize)
             }
             return instance!!
         }
+    }
+
+    @Synchronized
+    fun estaEmExecucao(): Boolean {
+        if(!estaExecutando.get()){
+            estaExecutando.set(true)
+            return false
+        }
+        return estaExecutando.get()
     }
 
     fun addTask(name: String, task: Runnable) {
@@ -26,16 +38,19 @@ class ParallelExecutor private constructor(threadPoolSize: Int) {
     }
 
     fun executeTasks() {
-        for (task in tasks) {
-            executor.execute {
-                try {
-                    task.runnable.run()
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    System.err.println("Exceção ignorada na tarefa '${task.name}': ${e.message}")
+        if (!estaEmExecucao()) {
+            while (tasks.isNotEmpty()) {
+                val task = tasks.poll()
+                executor.execute {
+                    try {
+                        task.runnable.run()
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        System.err.println("Exceção ignorada na tarefa '${task.name}': ${e.message}")
+                    }
                 }
             }
+            estaExecutando.set(false)
         }
-        executor.shutdown()
     }
 }
